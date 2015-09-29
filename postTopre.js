1b99d9e4b268e7b756361456767345949c6a991c
jigkoh3 <jigkoh3@hotmail.com>
2015-09-29 21:34:57 +0700

    update submit service

--- postTopre.js
+++ postTopre.js
@@ -3,10 +3,11 @@ var expect = require('chai').expect;
 var supertest = require('supertest');
 var api = supertest('http://172.19.192.116:9080');
 
-//http://172.19.192.116:9080/aftersales/tmv/migrateposttopre/validatemigrateposttopre?msisdn=0957730500
-describe('TDD Migrate PostToPre Normal Flow (' + msisdn + ')', function() {
 
-    var msisdn = "0957570002";
+//http://172.19.192.116:9080/aftersales/tmv/migrateposttopre/validatemigrateposttopre?msisdn=0957730500
+describe('TDD Migrate PostToPre Normal Flow ', function() {
+
+    var msisdn = "0957730501";
 
 
     //Data input for submit
@@ -43,16 +44,17 @@ describe('TDD Migrate PostToPre Normal Flow (' + msisdn + ')', function() {
             "engName": "ENGNAME",
             "shopcodes": ["80000084"],
             "logInName": "userTSM39"
-        }
+        };
         expect(saleAgent.logInName).to.not.equal("");
 
         payload.saleAgent = saleAgent;
+        done();
     });
 
     //Execute after getAuthen Success
     it('Generate OrderId', function(done) {
         //Assign partner_code from saleAgent
-        var partner_code = payload.saleAgent.partner_code[0];
+        var partner_code = payload.saleAgent.partnerCodes[0];
 
         var genIdTarget = '/aftersales/order/generate-id?channel=WEBUI&dealer=' + partner_code;
         api.get(genIdTarget)
@@ -70,12 +72,13 @@ describe('TDD Migrate PostToPre Normal Flow (' + msisdn + ')', function() {
                     "orderId": res.body["response-data"]
                 }
                 expect(res.body).to.not.have.property("display-messages");
+                console.log("trx-id : " + res.body["trx-id"] + " - " + res.body["status"]);
                 done();
             })
     });
 
     //Execute after generate-id Success
-    it('validatemigrateposttopre', function(done) {
+    it('validatemigrateposttopre with msisdn:' + msisdn, function(done) {
 
         var validateTarget = '/aftersales/tmv/migrateposttopre/validatemigrateposttopre?msisdn=' + msisdn;
 
@@ -84,7 +87,7 @@ describe('TDD Migrate PostToPre Normal Flow (' + msisdn + ')', function() {
             .set('Accept', 'application/json;charset=utf-8')
             .expect(200)
             .end(function(err, res) {
-
+                //console.log(res.body);
                 expect(res.body).to.have.property("status");
                 //Expect response-data is not null and contrain property customer
                 expect(res.body).to.have.property("response-data");
@@ -100,9 +103,15 @@ describe('TDD Migrate PostToPre Normal Flow (' + msisdn + ')', function() {
                 payload.productDetails = res.body["response-data"]["customer"]["installed-products"][0];
                 payload.customerAddress = res.body["response-data"]["customer"]["address-list"]["CUSTOMER_ADDRESS"];
                 delete payload.customerProfile['installed-products'];
+
+                //Fix value becuase migrate post to pre support personal only
+                payload.productDetails['account-category'] = "P";
+                payload.productDetails['account-sub-type'] = "PRE";
+
                 //Expect display-messages colection length == 0
                 expect(res.body).to.have.property("display-messages");
                 expect(res.body["display-messages"].length).to.equal(0);
+                console.log("trx-id : " + res.body["trx-id"] + " - " + res.body["status"]);
                 done();
             })
     });
@@ -116,7 +125,7 @@ describe('TDD Migrate PostToPre Normal Flow (' + msisdn + ')', function() {
         var customer_subtype = payload.productDetails["account-sub-type"]; // from validate
         var mobile_servicetype = payload.productDetails["mobile-servicetype"]; // from validate
         var propo_type = "NEW"; //??????
-        var partner_code = payload.saleAgent.partner_code[0];
+        var partner_code = payload.saleAgent.partnerCodes[0];
         var privilege = "false"; //?????
 
         var propositionTarget = '/sales/catalog/product/tmv/proposition/search?company-code=' + company_code + '&customer-type=' + customer_type + '&propo-type=' + propo_type + '&mobile-servicetype=' + mobile_servicetype + '&partner-code=' + partner_code + '&privilege=' + privilege;
@@ -138,7 +147,7 @@ describe('TDD Migrate PostToPre Normal Flow (' + msisdn + ')', function() {
 
                 //display-messages
                 expect(res.body).to.not.have.property("display-messages");
-
+                console.log("trx-id : " + res.body["trx-id"] + " - " + res.body["status"]);
                 done();
             })
     });
@@ -151,7 +160,7 @@ describe('TDD Migrate PostToPre Normal Flow (' + msisdn + ')', function() {
         var customer_subtype = payload.productDetails["account-sub-type"]; // from validate
         var mobile_servicetype = payload.productDetails["mobile-servicetype"]; // from validate
         var propo_type = "NEW"; //??????
-        var partner_code = payload.saleAgent.partner_code[0]; //from login sso
+        var partner_code = payload.saleAgent.partnerCodes[0]; //from login sso
         var privilege = "false"; //?????
 
         //receipt input proposition data from user
@@ -167,7 +176,7 @@ describe('TDD Migrate PostToPre Normal Flow (' + msisdn + ')', function() {
             .set('Accept', 'application/json;charset=utf-8')
             .expect(200)
             .end(function(err, res) {
-
+                //console.log(res.body)
                 expect(res.body).to.have.property("status");
                 expect(res.body).to.have.property("response-data");
                 expect(res.body["response-data"].length).to.above(0);
@@ -177,7 +186,7 @@ describe('TDD Migrate PostToPre Normal Flow (' + msisdn + ')', function() {
 
                 //display-messages
                 expect(res.body).to.not.have.property("display-messages");
-
+                console.log("trx-id : " + res.body["trx-id"] + " - " + res.body["status"]);
                 done();
             })
     });
@@ -195,6 +204,7 @@ describe('TDD Migrate PostToPre Normal Flow (' + msisdn + ')', function() {
         payload.customerProfile['birthdate'] = "1991-07-20T00:00:00+0700";
         payload.customerProfile['id-expire-date'] = "2016-07-20T00:00:00+0700";
         payload.customerProfile['language'] = "TH";
+        payload.customerProfile['gender'] = "MALE";
 
         payload.customerAddress['number'] = "61/238";
         payload.customerAddress['moo'] = "8";
@@ -230,6 +240,8 @@ describe('TDD Migrate PostToPre Normal Flow (' + msisdn + ')', function() {
 
         expect(payload.customerProfile).to.have.property("lastname");
         expect(payload.customerProfile['lastname']).to.not.equal("");
+
+        done();
     });
 
     //Execute after click confirm button
@@ -246,7 +258,7 @@ describe('TDD Migrate PostToPre Normal Flow (' + msisdn + ')', function() {
                     'title': payload.customerProfile['title'],
                     'firstname': payload.customerProfile['firstname'],
                     'lastname': payload.customerProfile['lastname'],
-                    //'gender': null,
+                    'gender': payload.customerProfile['gender'],
                     'id-type': payload.customerProfile['id-type'],
                     'id-number': payload.customerProfile['id-number'],
                     'birthdate': payload.customerProfile['birthdate'],
@@ -265,32 +277,32 @@ describe('TDD Migrate PostToPre Normal Flow (' + msisdn + ')', function() {
                     "address-list": {
                         "CUSTOMER_ADDRESS": payload.customerAddress
                     },
-                    // "customer-agents": {
-                    //     "AUTH_1": {
-                    //         "contact": "0868836665",
-                    //         "id-number": "9988877688845",
-                    //         "id-type": "I",
-                    //         "firstname": "สมคิด",
-                    //         "lastname": "คิดมากไป",
-                    //         "birthdate": "2015-07-20T00:00:00+0700"
-                    //     },
-                    //     "POA": {
-                    //         "contact": "0868836664",
-                    //         "id-number": "3257588733945",
-                    //         "id-type": "I",
-                    //         "firstname": "สมชาย",
-                    //         "lastname": "ปากสว่าง",
-                    //         "birthdate": "2015-07-20T00:00:00+0700"
-                    //     },
-                    //     "AUTH_2": {
-                    //         "contact": "0868836666",
-                    //         "id-number": "9988877687723",
-                    //         "id-type": "I",
-                    //         "firstname": "สมฤดี",
-                    //         "lastname": "ดีเกินไป",
-                    //         "birthdate": "2015-07-20T00:00:00+0700"
+                    //     "customer-agents": {
+                    //         "AUTH_1": {
+                    //             "contact": "0868836665",
+                    //             "id-number": "9988877688845",
+                    //             "id-type": "I",
+                    //             "firstname": "สมคิด",
+                    //             "lastname": "คิดมากไป",
+                    //             "birthdate": "2015-07-20T00:00:00+0700"
+                    //         },
+                    //         "POA": {
+                    //             "contact": "0868836664",
+                    //             "id-number": "3257588733945",
+                    //             "id-type": "I",
+                    //             "firstname": "สมชาย",
+                    //             "lastname": "ปากสว่าง",
+                    //             "birthdate": "2015-07-20T00:00:00+0700"
+                    //         },
+                    //         "AUTH_2": {
+                    //             "contact": "0868836666",
+                    //             "id-number": "9988877687723",
+                    //             "id-type": "I",
+                    //             "firstname": "สมฤดี",
+                    //             "lastname": "ดีเกินไป",
+                    //             "birthdate": "2015-07-20T00:00:00+0700"
+                    //         }
                     //     }
-                    // }
                 },
                 "sale-agent": {
                     'name': payload.saleAgent['engName'],
@@ -315,36 +327,10 @@ describe('TDD Migrate PostToPre Normal Flow (' + msisdn + ')', function() {
                         "BILLING_ADDRESS": payload.customerAddress,
                         "TAX_ADDRESS": payload.customerAddress
                     },
-                    // "order-data": {
-                    //     "IMSI": null,
-                    //     "MAXALLOW-APPROVE-CODE": null,
-                    //     "MOBILE-SERVICETYPE": null,
-                    //     "OFFERS-REQUIRE-PARAMETER": "CUG",
-                    //     $ {
-                    //         "OFFERS-REQUIRE-PARAMETER"
-                    //     } - PARAM - SIZE " : "
-                    //     2 ",
-                    //     $ {
-                    //         "OFFERS-REQUIRE-PARAMETER"
-                    //     } - PARAM - $ {
-                    //         SIZE
-                    //     }
-                    //     " : "
-                    //     NAME | VALUE | UNIT ",
-                    //     "SUBSCRIBER-TITLE-CODE": null,
-                    //     "SUBSCRIBER-TITLE": null,
-                    //     "SUBSCRIBER-FIRSTNAME": null,
-                    //     "SUBSCRIBER-LASTNAME": null,
-                    //     "SUBSCRIBER-BIRTHDATE": null,
-                    //     "SUBSCRIBER-GENDER": null,
-                    //     "SUBSCRIBER-SMS-LANG": null,
-                    //     "ACCOUNT-BILL-FORMAT": null,
-                    //     "ACCOUNT-EMAIL": null,
-                    //     "ACCOUNT-SMS-NUMBER": null,
-                    //     "ACCOUNT-PAYMENT-METHOD": null,
-                    //     "ACCOUNT-LANG": null,
-                    //     "ACCOUNT-BILL-CYCLE": null,
-                    // },
+                    "order-data": {
+                        "PRICEPLAN-SOC-CODE": payload.priceplanSelected["soc"],
+                        "CCBS-PROPOSITION-SOC-CODE": payload.propositionSelected['soc']
+                    },
                     "primary-order-data": {
                         "OU-ID": payload.productDetails['ouId'],
                         "BAN": payload.productDetails['ban'],
@@ -354,27 +340,126 @@ describe('TDD Migrate PostToPre Normal Flow (' + msisdn + ')', function() {
                         "NAS-PROPOSITION": payload.propositionSelected['proposition-code'],
                         "CCBS-PROPOSITION": payload.propositionSelected['name']
                             //"SIM": null
+                            //payload.propositionSelected['soc']
                     }
                 }],
                 "last-modify-date": ""
             },
             'ref-id': payload.orderData.TrxID,
             'user-id': payload.saleAgent.logInName,
-            'approver': "",
+            'approver': ""
         };
         api.post(submitTarget)
             .set('Accept', 'application/json;charset=utf-8')
             .send(request)
             .expect(200)
             .end(function(err, res) {
-                console.log(request.order['order-items'][0]);
-                console.log(res.body);
+                // console.log(request.order['order-items'][0]);
+                //console.log(res.body);
                 //status
                 expect(res.body).to.have.property("status");
                 expect(res.body.status).to.equal("SUCCESSFUL");
+                console.log("trx-id : " + res.body["trx-id"] + " - " + res.body["status"]);
                 done();
             })
     });
+
+
+});
+
+describe('TDD Migrate PostToPre Exception Flow  Case Not Allow Migrate', function() {
+
+
+    var msisdn = "0957730699";
+
+    //Data input for submit
+    //properties
+    // + saleAgent : object
+    // + orderData : object
+    // + customerProfile : object
+    // + productDetails : object
+    // + customerAddress : object
+    // + propositionSelected : object
+    // + priceplanSelected : object
+    var payload = {};
+
+    //Data collection from dropdown list & modal selector list
+    //properties
+    // + propositions : collection of object
+    // + priceplans : collection of object
+    // + titles : collection of object (but I don't have get service in this sernario)
+    // + resons : collection of object (but I don't have get service in this sernario)
+    var collectionData = {};
+
+    //Execute first on page load
+    it('get SSO Authen Header', function(done) {
+        var saleAgent = {
+            "shopType": "0",
+            "isSecondAuthen": true,
+            "isCorporate": true,
+            "channel": "WEBUI",
+            "partnerCodes": ['80000084'],
+            "partnerName": "xxxx",
+            "partnerType": "XX",
+            "saleCode": "00",
+            "thaiName": "THAINAME",
+            "engName": "ENGNAME",
+            "shopcodes": ["80000084"],
+            "logInName": "userTSM39"
+        };
+        expect(saleAgent.logInName).to.not.equal("");
+
+        payload.saleAgent = saleAgent;
+        done();
+    });
+
+    //Execute after getAuthen Success
+    it('Generate OrderId', function(done) {
+        //Assign partner_code from saleAgent
+        var partner_code = payload.saleAgent.partnerCodes[0];
+
+        var genIdTarget = '/aftersales/order/generate-id?channel=WEBUI&dealer=' + partner_code;
+        api.get(genIdTarget)
+            .set('Accept', 'application/json;charset=utf-8')
+            .expect(200)
+            .end(function(err, res) {
+
+                expect(res.body).to.have.property("status");
+                //Expect response-data
+                expect(res.body).to.have.property("response-data");
+
+                //Assign response-data to orderData
+                payload.orderData = {
+                    "TrxID": res.body["trx-id"],
+                    "orderId": res.body["response-data"]
+                }
+                expect(res.body).to.not.have.property("display-messages");
+                console.log("trx-id : " + res.body["trx-id"] + " - " + res.body["status"]);
+                done();
+            })
+    });
+
+    //Execute after generate-id Success
+    it('validatemigrateposttopre with msisdn:' + msisdn, function(done) {
+
+        var validateTarget = '/aftersales/tmv/migrateposttopre/validatemigrateposttopre?msisdn=' + msisdn;
+
+        api.get(validateTarget)
+            .set('roles', 'CN=R-TSM-IT FRIENDS,OU=Group,DC=ssoaddv,DC=th|CN=R-TSM-RETENTION,OU=Group,DC=ssoaddv,DC=th|CN=R-TSM-RR SERVICES,OU=Group,DC=ssoaddv,DC=th|CN=R-TSM-AFTER SALES DEVICES,OU=Group,DC=ssoaddv,DC=th|CN=R-TSM-RR SALES,OU=Group,DC=ssoaddv,DC=th')
+            .set('Accept', 'application/json;charset=utf-8')
+            .expect(200)
+            .end(function(err, res) {
+                //console.log(res.body);
+                expect(res.body).to.have.property("status");
+                //Expect display-messages colection length == 0
+                expect(res.body).to.have.property("display-messages");
+                expect(res.body["display-messages"].length).to.above(0);
+                console.log("trx-id : " + res.body["trx-id"] + " - " + res.body["status"]);
+                done();
+            })
+    });
+
+    
 
 
 });
